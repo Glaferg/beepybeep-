8c2ea28af3a4332b416ef90ae3061277ef9cc8fb
Glaferg <45836373+Glaferg@users.noreply.github.com>
2018-12-12 18:35:45 -0800

    a beepy first track that isnt great

---
+++ intense beeps_attack_release.rb
@@ -0,0 +1,18 @@
+# Welcome to Sonic Pi v3.1
+use_synth :pulse
+use_synth :saw
+use_synth :tb303
+play :a3, amp: 3.5, pan: -1,attack: 2, release: 3
+sleep 0.5
+play :b4, amp: 9.9, pan: 0, release: 3
+sleep 0.5
+play :c5, amp: 20.9, pan: 1
+use_synth :prophet
+sleep 0.5
+play :d5, amp: 50.0, pan: 1
+sleep 0.5
+play :g6, amp: 200.0, pan: -1
+sleep 0.6
+play :g6, amp: 200.0, pan: -1
+
+
